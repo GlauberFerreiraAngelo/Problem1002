ae104df37453462f31baa8545010fef02f944515
GlauberFerreiraAngelo <42098584+GlauberFerreiraAngelo@users.noreply.github.com>
2022-02-21 11:21:25 -0300

    Add files via upload

---
+++ Problem1002/src/com/problem/Main.java
@@ -0,0 +1,19 @@
+package com.problem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double pi = 3.14159;
+		double area;
+		double raio;
+		
+		raio = 100.64;
+		
+		area = pi * Math.pow(raio, 2);
+		
+		System.out.printf(" %.4f ",area);
+		
+		
+	}
+}
